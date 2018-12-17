cf949777b89a2da28416c7e422a13ffc23d17cc3
Carol Gardiner <carolan.gardiner@gmail.com>
2018-12-17 15:29:11 +0000

    Added method content to Message class

--- lib/message.rb
+++ lib/message.rb
@@ -1,9 +1,10 @@
 class Message
 
-attr_reader :title
+attr_reader :title, :content
 
-def initialize(title)
+def initialize(title, content)
   @title = title
+  @content = content
 end
 
 end
--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -1,8 +1,9 @@
 require "message"
 
 describe "#new" do
-  it "it initializes with a title" do
-    m = Message.new("Carol")
+  it "it initializes with a title and content" do
+    m = Message.new("Carol", "This is content")
     expect(m.title).to eq("Carol")
+    expect(m.content).to eq ("This is content")
   end
 end
