323aa9de0d5ea5aa8bda3c54f8907a89d34c7395
Simon Cass <scass1991@gmail.com>
2018-12-17 14:04:36 +0000

    pushing to switch partners

--- spec/features/uservisit_spec.rb
+++ spec/features/uservisit_spec.rb
@@ -1,6 +1,12 @@
 feature "user can visit site & see content" do
+
   scenario "user can see header" do
     visit "/"
     expect(page).to have_content("Welcome to MSNGR")
+  end
+
+  scenario "user can see a form" do
+    visit "/"
+    expect(page).to find_field("MSGS").value
   end
 end
