d443d26d6af2b578dbd3adba2e3c6f1b6d62c0af
Carol Gardiner <carolan.gardiner@gmail.com>
2018-12-17 14:41:22 +0000

    Passing feature test to fill in message form

--- app.rb
+++ app.rb
@@ -3,7 +3,14 @@ require "sinatra/base"
 class MSNGR < Sinatra::Base
 
   get "/" do
-    erb :index
+    erb(:index)
   end
+
+  post "/post_message" do
+    p @msg_cheese = params[:msg_name]
+    p @msg_text = params[:user_message]
+    erb(:message)
+  end
+
 
 end
--- spec/features/uservisit_spec.rb
+++ spec/features/uservisit_spec.rb
@@ -1,12 +1,14 @@
 feature "user can visit site & see content" do
-
   scenario "user can see header" do
-    visit "/"
+    visit("/")
     expect(page).to have_content("Welcome to MSNGR")
   end
 
-  scenario "user can see a form" do
-    visit "/"
-    expect(page).to find_field("MSGS").value
+  scenario 'user can fill in a form and submit' do 
+    visit("/")
+    fill_in 'msg_name', with: "carol" 
+    click_button('submit')
+    expect(page).to have_content("carol")
   end
 end
+
